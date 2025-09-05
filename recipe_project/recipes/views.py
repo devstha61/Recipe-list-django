3da4e59dcedd1668d8c5b78272e403e128c062b5
Dev <nameofdeev@gmail.com>
2025-09-05 19:39:35 +0545

    Added user authentication and receipe_detail

--- recipe_project/recipes/views.py
+++ recipe_project/recipes/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Recipe
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
-    return render(request, "../templates/recipe_list.html", {'recipes': recipes})
+    return render(request, 'recipe_list.html', {'recipes': recipes})
 
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) 
+            return redirect('recipe-list')
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {'form': form})
